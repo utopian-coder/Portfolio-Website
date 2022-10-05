fb1e3758454bd59bfad1ad127797a69ef903e3d4
Imran Biswas <imranbiswas2209@gmail.com>
2022-10-05 21:39:58 +0530

    Header section completed!

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Imran Biswas</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className='me'>
+          <img src={ME} alt='profile' />
+        </div>
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
